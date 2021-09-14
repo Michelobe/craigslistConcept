8f78c7262357231fef77d2c245f568da72a12775
Michael Sean De La Riva <michaeldlr17@gmail.com>
2021-09-14 10:55:24 -0700

    changing the name of the project both on local and remote repositories. This commit is a test to make sure I am able to push to github.  -Michael De La Riva

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
     render() {
         return (
             <Router>
+                
                 <div className="App">
                         <Header />
                         <Route exact path = '/'>
